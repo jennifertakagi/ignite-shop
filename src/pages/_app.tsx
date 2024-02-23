8ac19012a82ca01e45cd076c4b60c9ae4351fdb0
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-23 15:12:06 +0100

    [feat] Add Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logoImg from "../assets/logo.png"
+import { Container, Header } from "../styles/pages/app"
+
 globalStyles()
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img style={{width: 80 }} src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default App
