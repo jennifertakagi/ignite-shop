4084dd15de5365c7d723c18f79eee0c17a19a2ea
Jennifer Takagi <jennifer.takagi@personio.de>
2024-02-24 11:18:02 +0100

    [feat] Add Home structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,21 @@
-import { styled } from "../styles"
-
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold'
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)'
-  }
-})
+import { HomeContainer, Product } from "@/styles/pages/home";
 
 export default function Home() {
   return (
-    <Button>
-      <span>Testing a button</span>
-      send
-    </Button>
+    <HomeContainer>
+      <Product>
+        <footer>
+          <strong>T-shirt X</strong>
+            <span>$ 79.90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <footer>
+        <strong>T-shirt X</strong>
+          <span>$ 79.90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
